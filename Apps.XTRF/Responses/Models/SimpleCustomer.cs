cf51b8fd50e49ce2a347576f40e4e7a167a96c7b
bohdanm-bb <bohdan.makarenko@blackbird.io>
2023-07-07 13:22:54 +0300

    Nested object test

--- Apps.XTRF/Responses/Models/SimpleCustomer.cs
+++ Apps.XTRF/Responses/Models/SimpleCustomer.cs
@@ -8,6 +8,22 @@ namespace Apps.XTRF.Responses.Models
 {
     public class SimpleCustomer
     {
+        public SimpleCustomer()
+        {
+            Child = new ChildCustomer()
+            {
+                Id = 1234,
+                Name = "Test child name",
+            };
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ChildCustomer Child { get; set; }
+    }
+
+    public class ChildCustomer
+    {
         public int Id { get; set; }
         public string Name { get; set; }
     }
