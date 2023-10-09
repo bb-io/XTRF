33c55280b466c9031b464166457b52c5f1c1e3c4
bZverok <bohdan.zverok@blackbird.io>
2023-10-09 11:25:29 +0300

    updated file categories

--- Apps.XTRF/DataSourceHandlers/EnumHandlers/FileCategoryDataHandler.cs
+++ Apps.XTRF/DataSourceHandlers/EnumHandlers/FileCategoryDataHandler.cs
@@ -6,21 +6,21 @@ public class FileCategoryDataHandler : EnumDataHandler
 {
     protected override Dictionary<string, string> EnumValues => new()
     {
-        { "BILINGUAL_DOCUMENT", "Bilingual document" },
+        { "BILINGUAL_DOC", "Bilingual document" },
         { "CAT_ANALYSIS", "CAT analysis" },
         { "CAT_PACKAGE", "CAT package" },
         { "CAT_PACKAGE_RETURN", "CAT package (return)" },
         { "FILTERING_RULES", "Filtering rules" },
         { "FORMATTED_DOCUMENT", "Formatted document" },
-        { "MEMOQ_LIGHT_RESOURCE", "memoQ light resource" },
+        { "MEMOQ_LIGHT_RESOURCES", "memoQ light resource" },
         { "OTHER", "Other" },
         { "QA_REPORT", "QA report" },
-        { "REFERENCE_FILE", "Reference file" },
+        { "REFERENCE", "Reference file" },
         { "SEGMENTATION_RULES", "Segmentation rules" },
         { "SOURCE_DOCUMENT", "Source document" },
         { "SOURCE_TO_BE_PREPARED", "Source to be prepared" },
         { "TERMINOLOGY", "Terminology" },
         { "TRANSLATED_DOCUMENT", "Translated document" },
-        { "TRANSLATION_MEMORY", "Translation memory" }
+        { "TM", "Translation memory" }
     };
 }
