2d8d952bcb9f4f0f91b2b66e1dfae22a1c32645f
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-03-27 15:40:55 +0200

    Made bool not nullable

--- Apps.XTRF/Shared/Models/Entities/CustomField.cs
+++ Apps.XTRF/Shared/Models/Entities/CustomField.cs
@@ -4,4 +4,4 @@ public record CustomField(string Type, string Name, string Key);
 
 public record CustomField<T>(string Type, string Name, string Key, T? Value) : CustomField(Type, Name, Key);
 
-public record CustomBooleanField(string Type, string Key, string Name, bool? Value);
+public record CustomBooleanField(string Type, string Key, string Name, bool Value);
