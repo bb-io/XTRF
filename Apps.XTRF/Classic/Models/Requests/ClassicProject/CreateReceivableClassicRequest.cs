b1aa67111f07ce627c76fd11c7b403d7cc4c9f22
vitalii-bezuhlyi <vitalii.bezuhlyi@blackbird.io>
2024-04-02 17:45:03 +0300

    Improved inputs for create payable request

--- Apps.XTRF/Classic/Models/Requests/ClassicProject/CreateReceivableClassicRequest.cs
+++ Apps.XTRF/Classic/Models/Requests/ClassicProject/CreateReceivableClassicRequest.cs
@@ -74,6 +74,6 @@ public class CreateClassicPayableRequest : CreateClassicFinanceRequest
     [Display("Payable ID")]
     public string? Id { get; set; }
     
-    [Display("Job ID"), DataSource(typeof(SmartJobDataHandler))]
+    [Display("Job ID", Description = "Job ID associated with the payable. You can locate it in the job section. Click on the job and check the URL for '&id=270'.")]    
     public string JobId { get; set; }
 }
