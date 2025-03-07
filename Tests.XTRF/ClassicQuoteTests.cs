45cb584d40269bc2f2fddd532d7ea03ca6fec7ba
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-03-07 17:33:13 +0100

    Rework custom fields

---
+++ Tests.XTRF/ClassicQuoteTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apps.XTRF.Classic.Actions;
+using XTRF.Base;
+
+namespace Tests.XTRF
+{
+    [TestClass]
+    public class ClassicQuoteTests :TestBase
+    {
+        [TestMethod]
+        public async Task GetQuote_ValidInput_ReturnsResult()
+        {
+            var action = new ClassicQuoteActions(InvocationContext, FileManager);
+            var response = await action.GetQuote(new Apps.XTRF.Shared.Models.Identifiers.QuoteIdentifier()
+            {
+                QuoteId = "fakeId"
+            });
+
+            Assert.IsNotNull(response);
+        }
+    }
+}
---
+++ Tests.XTRF/ProvidePersonTests .cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apps.XTRF.Classic.Actions;
+using Apps.XTRF.Shared.Actions;
+using XTRF.Base;
+
+namespace Tests.XTRF
+{
+    [TestClass]
+    public class ProvidePersonTests :TestBase
+    {
+        [TestMethod]
+        public async Task SearchProviderPersons_ValidInput_ReturnsResult()
+        {
+            var action = new ProviderPersonActions(InvocationContext);
+            var response = await action.SearchProviderPersonsAsync(new Apps.XTRF.Shared.Models.Requests.Provider.ProviderPersonSearchRequest()
+            {
+                Email = "testvendor@blackbird.io"
+            });
+
+            Assert.IsNotNull(response);
+        }
+    }
+}
