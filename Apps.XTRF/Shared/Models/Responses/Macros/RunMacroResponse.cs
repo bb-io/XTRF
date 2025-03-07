c34fbc4a0b0bf0f6f65a9416a96f9fc060bd168c
RiabushenkoA <ariabushenko@blackbird.io>
2025-03-07 18:29:21 +0200

    Added action 'Run macro'

---
+++ Apps.XTRF/Shared/Models/Responses/Macros/RunMacroResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.XTRF.Shared.Models.Responses.Macros
+{
+    public class RunMacroResponse
+    {
+        [Display("Action ID")]
+        public string ActionId { get; set; }
+
+        [Display("Status URL")]
+        public string StatusUrl { get; set; }
+
+        [Display("Result URL")]
+        public string ResultUrl { get; set; }
+    }
+}
---
+++ Tests.XTRF/MacrosTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Apps.XTRF.Shared.Actions;
+using Apps.XTRF.Shared.Models.Requests.Macros;
+using XTRF.Base;
+
+namespace Tests.XTRF
+{
+    [TestClass]
+    public class MacrosTests : TestBase
+    {
+        [TestMethod]
+        public async Task RunMacros_IsSucces()
+        {
+            var action = new ProviderActions(InvocationContext);
+
+            var response = await action.RunMacroAsync(new RunMacroRequest
+            {
+                MacroId = "1456546",
+                //ItemIds = new List<string> { "1", "2" }
+            });
+
+            Console.WriteLine(response.StatusUrl);
+            Assert.IsNotNull(response);
+        }
+    }
+}
