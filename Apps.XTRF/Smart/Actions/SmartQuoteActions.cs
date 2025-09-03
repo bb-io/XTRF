15c2f9ffaea8762870e46c217a1e7dbf0536c3bf
ce-nistal <cecilia.nistal@blackbird.io>
2025-09-03 11:30:36 -0300

    Create project returns empty list of tasks instead of null in case of no tasks

--- Apps.XTRF/Smart/Actions/SmartQuoteActions.cs
+++ Apps.XTRF/Smart/Actions/SmartQuoteActions.cs
@@ -55,49 +55,6 @@ public class SmartQuoteActions : BaseFileActions
         return quoteResponse;
     }
 
-    [Action("Smart: Add receivable to quote",
-      Description = "Adds a receivable line")]
-    public async Task<QuoteIdentifier> AddReceivableToQuote(
-      [ActionParameter] QuoteIdentifier quoteIdentifier,
-      [ActionParameter] AddQuoteReceivableRequest input)
-    {
-        if (string.IsNullOrWhiteSpace(input.CalculationUnitId))
-            throw new PluginMisconfigurationException("Calculation Unit ID is required.");
-        if (input.Units <= 0)
-            throw new PluginMisconfigurationException("Units must be greater than zero.");
-
-        var payload = new
-        {
-            languageCombination = input.SourceLanguageId != null && input.TargetLanguageId != null
-                ? new
-                {
-                    sourceLanguageId = ConvertToInt64(input.SourceLanguageId, "Source language"),
-                    targetLanguageId = ConvertToInt64(input.TargetLanguageId, "Target language")
-                }
-                : null,
-            languageCombinationIdNumber = input.LanguageCombinationIdNumber,
-            jobTypeId = string.IsNullOrWhiteSpace(input.JobTypeId)
-                ? null
-                : ConvertToInt64(input.JobTypeId, "Job type"),
-
-            type = input.Type ?? "SIMPLE",
-            calculationUnitId = ConvertToInt64(input.CalculationUnitId, "Calculation unit"),
-            quantity = input.Units,
-            rate = input.Rate,
-            rateOrigin = input.Rate.HasValue ? "CUSTOM" : "PRICE_PROFILE",
-            currencyId = string.IsNullOrWhiteSpace(input.CurrencyId) ? null : ConvertToInt64(input.CurrencyId, "Currency"),
-            ignoreMinimumCharge = input.IgnoreMinimumCharge,
-            minimumCharge = input.MinimumCharge,
-            description = input.Description
-        };
-
-        var req = new XtrfRequest($"/v2/quotes/{quoteIdentifier.QuoteId}/finance/receivables",
-            Method.Post, Creds).WithJsonBody(payload, JsonConfig.Settings);
-
-        await Client.ExecuteWithErrorHandling(req);
-        return quoteIdentifier;
-    }
-
     [Action("Smart: List jobs in quote", Description = "List all jobs in a smart quote")]
     public async Task<ListJobsResponse> GetJobsByQuote([ActionParameter] QuoteIdentifier quoteIdentifier)
     {
@@ -194,6 +151,48 @@ public class SmartQuoteActions : BaseFileActions
         await Client.ExecuteWithErrorHandling(addFileRequest);
     }
 
+    [Action("Smart: Add receivable to quote",
+      Description = "Adds a receivable line")]
+    public async Task<QuoteIdentifier> AddReceivableToQuote(
+      [ActionParameter] QuoteIdentifier quoteIdentifier,
+      [ActionParameter] AddQuoteReceivableRequest input)
+    {
+        if (string.IsNullOrWhiteSpace(input.CalculationUnitId))
+            throw new PluginMisconfigurationException("Calculation Unit ID is required.");
+        if (input.Units <= 0)
+            throw new PluginMisconfigurationException("Units must be greater than zero.");
+
+        var payload = new
+        {
+            languageCombination = input.SourceLanguageId != null && input.TargetLanguageId != null
+                ? new
+                {
+                    sourceLanguageId = ConvertToInt64(input.SourceLanguageId, "Source language"),
+                    targetLanguageId = ConvertToInt64(input.TargetLanguageId, "Target language")
+                }
+                : null,
+            languageCombinationIdNumber = input.LanguageCombinationIdNumber,
+            jobTypeId = string.IsNullOrWhiteSpace(input.JobTypeId)
+                ? null
+                : ConvertToInt64(input.JobTypeId, "Job type"),
+
+            type = input.Type ?? "SIMPLE",
+            calculationUnitId = ConvertToInt64(input.CalculationUnitId, "Calculation unit"),
+            quantity = input.Units,
+            rate = input.Rate,
+            rateOrigin = input.Rate.HasValue ? "CUSTOM" : "PRICE_PROFILE",
+            currencyId = string.IsNullOrWhiteSpace(input.CurrencyId) ? null : ConvertToInt64(input.CurrencyId, "Currency"),
+            ignoreMinimumCharge = input.IgnoreMinimumCharge,
+            minimumCharge = input.MinimumCharge,
+            description = input.Description
+        };
+
+        var req = new XtrfRequest($"/v2/quotes/{quoteIdentifier.QuoteId}/finance/receivables",
+            Method.Post, Creds).WithJsonBody(payload, JsonConfig.Settings);
+
+        await Client.ExecuteWithErrorHandling(req);
+        return quoteIdentifier;
+    }
     #endregion
 
     #region Put
