867fb059d28ae609e7e7b543404851789475699b
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-10-24 18:57:57 +0300

    use file-scoped namespace in DataSources tests

--- Tests.XTRF/DataSources.cs
+++ Tests.XTRF/DataSources.cs
@@ -7,95 +7,94 @@ using Apps.XTRF.Smart.Models.Requests.SmartQuote;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 using XTRF.Base;
 
-namespace Tests.XTRF
+namespace Tests.XTRF;
+
+[TestClass]
+public class DataSources : TestBase
 {
-    [TestClass]
-    public class DataSources : TestBase
+    [TestMethod]
+    public async Task DictionaryDataHandlerReturnsValues()
     {
-        [TestMethod]
-        public async Task DictionaryDataHandlerReturnsValues()
+        var dataHandler = new ProviderInvoiceDataHandler(InvocationContext);
+
+        var result = await dataHandler.GetDataAsync(new DataSourceContext(), CancellationToken.None);
+
+        foreach (var item in result)
         {
-            var dataHandler = new ProviderInvoiceDataHandler(InvocationContext);
-
-            var result = await dataHandler.GetDataAsync(new DataSourceContext(), CancellationToken.None);
-
-            foreach (var item in result)
-            {
-                Console.WriteLine($"{item.Key} - {item.Value}");
-                Assert.IsTrue(item.Key != null);
-            }
+            Console.WriteLine($"{item.Key} - {item.Value}");
+            Assert.IsTrue(item.Key != null);
         }
+    }
 
-        [TestMethod]
-        public async Task SmartServiceDataSourceHandlerReturnsValues()
+    [TestMethod]
+    public async Task SmartServiceDataSourceHandlerReturnsValues()
+    {
+        var dataHandler = new SmartServiceDataSourceHandler(InvocationContext);
+
+        var result = await dataHandler.GetDataAsync(new DataSourceContext(), CancellationToken.None);
+
+        foreach (var item in result)
         {
-            var dataHandler = new SmartServiceDataSourceHandler(InvocationContext);
-
-            var result = await dataHandler.GetDataAsync(new DataSourceContext(), CancellationToken.None);
-
-            foreach (var item in result)
-            {
-                Console.WriteLine($"{item.Key} - {item.Value}");
-                Assert.IsTrue(item.Key != null);
-            }
+            Console.WriteLine($"{item.Key} - {item.Value}");
+            Assert.IsTrue(item.Key != null);
         }
+    }
 
-        [TestMethod]
-        public async Task JobTypesReturnsValues()
+    [TestMethod]
+    public async Task JobTypesReturnsValues()
+    {
+        var dataHandler = new JobTypeDataHandler(InvocationContext);
+
+        var result = await dataHandler.GetDataAsync(new DataSourceContext(), CancellationToken.None);
+
+        foreach (var item in result)
         {
-            var dataHandler = new JobTypeDataHandler(InvocationContext);
-
-            var result = await dataHandler.GetDataAsync(new DataSourceContext(), CancellationToken.None);
-
-            foreach (var item in result)
-            {
-                Console.WriteLine($"{item.Key} - {item.Value}");
-                Assert.IsTrue(item.Key != null);
-            }
+            Console.WriteLine($"{item.Key} - {item.Value}");
+            Assert.IsTrue(item.Key != null);
         }
+    }
 
-        [TestMethod]
-        public async Task JobTypesNamesReturnsValues()
+    [TestMethod]
+    public async Task JobTypesNamesReturnsValues()
+    {
+        var dataHandler = new JobTypeNameDataHandler(InvocationContext);
+
+        var result = await dataHandler.GetDataAsync(new DataSourceContext(), CancellationToken.None);
+
+        foreach (var item in result)
         {
-            var dataHandler = new JobTypeNameDataHandler(InvocationContext);
-
-            var result = await dataHandler.GetDataAsync(new DataSourceContext(), CancellationToken.None);
-
-            foreach (var item in result)
-            {
-                Console.WriteLine($"{item.Key} - {item.Value}");
-                Assert.IsTrue(item.Key != null);
-            }
+            Console.WriteLine($"{item.Key} - {item.Value}");
+            Assert.IsTrue(item.Key != null);
         }
+    }
 
-        [TestMethod]
-        public async Task JobStatusReturnsValues()
+    [TestMethod]
+    public async Task JobStatusReturnsValues()
+    {
+        var dataHandler = new JobStatusDataHandler(InvocationContext, new JobIdentifier { JobId = "FGMD2QMQDZA6FA3CRINMZKYF64" }
+            );
+
+        var result = await dataHandler.GetDataAsync(new DataSourceContext(), CancellationToken.None);
+
+        foreach (var item in result)
         {
-            var dataHandler = new JobStatusDataHandler(InvocationContext, new JobIdentifier { JobId = "FGMD2QMQDZA6FA3CRINMZKYF64" }
-                );
-
-            var result = await dataHandler.GetDataAsync(new DataSourceContext(), CancellationToken.None);
-
-            foreach (var item in result)
-            {
-                Console.WriteLine($"{item.Key} - {item.Value}");
-                Assert.IsTrue(item.Key != null);
-            }
+            Console.WriteLine($"{item.Key} - {item.Value}");
+            Assert.IsTrue(item.Key != null);
         }
+    }
 
-        [TestMethod]
-        public async Task SmartQuoteCustomFieldDataHandlerReturnsValues()
-        {
-            // Arrange
-            var quote = new SmartQuoteIdentifier { QuoteId = "AHMG2QPUCBE6XGTB7XLDZ7R4AI" };
-            var handler = new SmartQuoteCustomFieldDataHandler(quote, InvocationContext);
+    [TestMethod]
+    public async Task SmartQuoteCustomFieldDataHandlerReturnsValues()
+    {
+        // Arrange
+        var quote = new SmartQuoteIdentifier { QuoteId = "AHMG2QPUCBE6XGTB7XLDZ7R4AI" };
+        var handler = new SmartQuoteCustomFieldDataHandler(quote, InvocationContext);
 
-            // Act
-            var result = await handler.GetDataAsync(new DataSourceContext(), CancellationToken.None);
+        // Act
+        var result = await handler.GetDataAsync(new DataSourceContext(), CancellationToken.None);
 
-            // Assert
-            foreach (var item in result)
-                Console.WriteLine($"{item.DisplayName}");
-        }
+        // Assert
+        foreach (var item in result)
+            Console.WriteLine($"{item.DisplayName}");
     }
 }
