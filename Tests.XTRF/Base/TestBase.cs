2b7f9968a9ce20d26b524864aba6e33af01521d7
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-10-24 12:09:30 +0300

    add TestBase.PrintJsonResult, refactor SmartQuoteActionTests, use file-scoped namespace

--- Tests.XTRF/Base/TestBase.cs
+++ Tests.XTRF/Base/TestBase.cs
@@ -1,34 +1,39 @@
 ﻿using Blackbird.Applications.Sdk.Common.Authentication;
 using Blackbird.Applications.Sdk.Common.Invocation;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
 
-namespace XTRF.Base
+namespace XTRF.Base;
+
+public class TestBase
 {
-    public class TestBase
+    public IEnumerable<AuthenticationCredentialsProvider> Creds { get; set; }
+
+    public InvocationContext InvocationContext { get; set; }
+
+    public FileManager FileManager { get; set; }
+
+    public TestBase()
     {
-        public IEnumerable<AuthenticationCredentialsProvider> Creds { get; set; }
+        var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+        Creds = config.GetSection("ConnectionDefinition").GetChildren()
+            .Select(x => new AuthenticationCredentialsProvider(x.Key, x.Value)).ToList();
 
-        public InvocationContext InvocationContext { get; set; }
 
-        public FileManager FileManager { get; set; }
+        var relativePath = config.GetSection("TestFolder").Value;
+        var projectDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+        var folderLocation = Path.Combine(projectDirectory, relativePath);
 
-        public TestBase()
+        InvocationContext = new InvocationContext
         {
-            var config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
-            Creds = config.GetSection("ConnectionDefinition").GetChildren()
-                .Select(x => new AuthenticationCredentialsProvider(x.Key, x.Value)).ToList();
+            AuthenticationCredentialsProviders = Creds,
+        };
 
+        FileManager = new FileManager();
+    }
 
-            var relativePath = config.GetSection("TestFolder").Value;
-            var projectDirectory = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
-            var folderLocation = Path.Combine(projectDirectory, relativePath);
-
-            InvocationContext = new InvocationContext
-            {
-                AuthenticationCredentialsProviders = Creds,
-            };
-
-            FileManager = new FileManager();
-        }
+    protected static void PrintJsonResult(object result)
+    {
+        Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
     }
 }
