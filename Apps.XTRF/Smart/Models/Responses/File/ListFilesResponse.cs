575cd3560dd8c836c3012e02dbc308f3d58ab0e3
Alex Terehov <terales@users.noreply.github.com>
2025-10-27 16:15:09 -0230

    Add file names array to files response

--- Apps.XTRF/Smart/Models/Responses/File/ListFilesResponse.cs
+++ Apps.XTRF/Smart/Models/Responses/File/ListFilesResponse.cs
@@ -1,5 +1,10 @@
 ﻿using Apps.XTRF.Smart.Models.Entities;
+using Blackbird.Applications.Sdk.Common;
 
 namespace Apps.XTRF.Smart.Models.Responses.File;
 
-public record ListFilesResponse(IEnumerable<SmartFileXTRF> Files);
+public record ListFilesResponse(IEnumerable<SmartFileXTRF> Files)
+{
+    [Display("File names")]
+    IEnumerable<string> FileNames => Files.Select(f => f.Name);
+};
