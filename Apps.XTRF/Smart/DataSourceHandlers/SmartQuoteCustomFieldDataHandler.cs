44d92833c8b560c9c65f05e1212bc0e5933f10ae
Anton Fesenko <tohafesenkolovep@gmail.com>
2025-10-27 10:10:02 +0200

    add SmartQuoteCustomFieldDataHandler to SmartCustomFieldIdentifier, remove SmartQuoteIdentifier

--- Apps.XTRF/Smart/DataSourceHandlers/SmartQuoteCustomFieldDataHandler.cs
+++ Apps.XTRF/Smart/DataSourceHandlers/SmartQuoteCustomFieldDataHandler.cs
@@ -1,13 +1,13 @@
 ﻿using Apps.XTRF.Shared.DataSourceHandlers;
 using Apps.XTRF.Shared.Models.Entities.Enums;
-using Apps.XTRF.Smart.Models.Requests.SmartQuote;
+using Apps.XTRF.Shared.Models.Identifiers;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 using Blackbird.Applications.Sdk.Common.Invocation;
 
-namespace Apps.XTRF.Smart.Actions;
+namespace Apps.XTRF.Smart.DataSourceHandlers;
 
-public class SmartQuoteCustomFieldDataHandler([ActionParameter] SmartQuoteIdentifier quote, InvocationContext invocationContext)
+public class SmartQuoteCustomFieldDataHandler([ActionParameter] QuoteIdentifier quote, InvocationContext invocationContext)
     : BaseQuoteCustomFieldDataHandler(quote.QuoteId, ApiType.Smart, invocationContext), IAsyncDataSourceItemHandler
 {
 }
--- Apps.XTRF/Smart/Models/Requests/SmartQuote/SmartCustomFieldIdentifier.cs
+++ Apps.XTRF/Smart/Models/Requests/SmartQuote/SmartCustomFieldIdentifier.cs
@@ -1,12 +1,12 @@
-﻿using Apps.XTRF.Smart.Actions;
+﻿using Apps.XTRF.Smart.DataSourceHandlers;
 using Blackbird.Applications.Sdk.Common;
 using Blackbird.Applications.Sdk.Common.Dynamic;
 
 namespace Apps.XTRF.Smart.Models.Requests.SmartQuote;
 
-public class SmartQuoteIdentifier
+public class SmartCustomFieldIdentifier
 {
-    [Display("Quote ID")]
     [DataSource(typeof(SmartQuoteCustomFieldDataHandler))]
-    public string QuoteId { get; set; }
+    [Display("Custom field key")]
+    public string Key { get; set; }
 }
