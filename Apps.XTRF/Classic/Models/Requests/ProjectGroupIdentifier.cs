7b4d4d3cf3cb41822086218cc9b7be681cc7962a
Artem Riabushenko <ariabushenko@blackbird.io>
2026-03-09 20:31:20 +0200

    Added action to assign projects and quotes to project groups

---
+++ Apps.XTRF/Classic/Models/Requests/ProjectGroupIdentifier.cs
@@ -0,0 +1,10 @@
+﻿using Blackbird.Applications.Sdk.Common;
+
+namespace Apps.XTRF.Classic.Models.Requests
+{
+    public class ProjectGroupIdentifier
+    {
+        [Display("Project group ID")]
+        public string ProjectGroupId { get; set; } = string.Empty;
+    }
+}
