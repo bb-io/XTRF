973da10f5de9e7a40a3bc0bc4dfd2d8c4207ba2e
Artem Riabushenko <ariabushenko@blackbird.io>
2026-04-06 20:16:43 +0300

    Adjustments response model for quote

--- Tests.XTRF/ClassicQuoteTests.cs
+++ Tests.XTRF/ClassicQuoteTests.cs
@@ -1,26 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Apps.XTRF.Classic.Actions;
+﻿using Apps.XTRF.Classic.Actions;
+using Apps.XTRF.Classic.Models.Requests.ClassicQuote;
 using XTRF.Base;
 
-namespace Tests.XTRF
-{
-    [TestClass]
-    public class ClassicQuoteTests :TestBase
-    {
-        [TestMethod]
-        public async Task GetQuote_ValidInput_ReturnsResult()
-        {
-            var action = new ClassicQuoteActions(InvocationContext, FileManager);
-            var response = await action.GetQuote(new Apps.XTRF.Shared.Models.Identifiers.QuoteIdentifier()
-            {
-                QuoteId = "fakeId"
-            });
+namespace Tests.XTRF;
 
-            Assert.IsNotNull(response);
-        }
+[TestClass]
+public class ClassicQuoteTests : TestBase
+{
+    [TestMethod]
+    public async Task GetQuote_ValidInput_ReturnsResult()
+    {
+        var action = new ClassicQuoteActions(InvocationContext, FileManager);
+        var response = await action.GetQuote(new Apps.XTRF.Shared.Models.Identifiers.QuoteIdentifier()
+        {
+            QuoteId = "fakeId"
+        });
+
+        Assert.IsNotNull(response);
+    }
+
+    [TestMethod]
+    public async Task CreateQuote_ReturnsResult()
+    {
+        var action = new ClassicQuoteActions(InvocationContext, FileManager);
+        var response = await action.CreateQuote(new QuoteCreateRequest()
+        {
+            Note= "Quote generated automatically from Hubspot on 2026-04-04T18:28:24.264Z",
+            PersonId = "60015",
+            QuoteName = "Devis TERAGIR - ALIX",
+            ServiceId = "30",
+            PriceProfileId = "33920",
+            SourceLanguageId = "87",
+            SpecializationId = "154",
+        });
+
+        Assert.IsNotNull(response);
     }
 }
